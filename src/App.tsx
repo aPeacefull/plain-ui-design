d6655b441d8b68c9385c58036eb88a29473fd3a6
Anastasia Peacefull <aPeacefull@users.noreply.github.com>
2024-08-12 15:18:48 -0700

    added card component
    card component with two variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { GlobalStyle, themeLight } from "./lib/theme";
 import { Placeholder, ButtonRectangular } from "./lib";
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import { Icon } from "./lib/icon";
+import { Card } from "./lib";
+
+ const StyledMain = styled.main`
+	display: flex;
+	flex-direction: row;
+	background-color: #F4F3F1;
+	gap: 52px;
+	padding: 52px;
+	heght: 100%;
+ `;
+
+
 
 function App() {
 	const switcher = false;
 	return (
 		<ThemeProvider theme={themeLight}>
 			<GlobalStyle />
-			<main>
-				<Placeholder text="Игеещты" />
-				<ButtonRectangular text="Large primary enabled" appearance="primary" size="large" leadingElement={<Icon />} />
+			<StyledMain>
+			  <Card breakpoint="LM" text = "Building semantic Design System"/>
+				<br />
+				<Card breakpoint="S" text = "Building semantic Design System"/>
 				
-				
-			</main>
+			</StyledMain>
 		</ThemeProvider>
 	);
 }
